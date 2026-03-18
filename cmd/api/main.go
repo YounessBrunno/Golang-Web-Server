b39f9d7c0707dac84054a6e4bc02b8d214d53f45
youness oukhadjou <brunnotools@gmail.com>
2026-03-18 22:31:12 +0000

    implemented a simple file server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,11 @@ import (
 
 func main() {
 
- fmt.Println("Hello, World!");
+  fileServer := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fileServer)
+  
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+  })
 
 }
